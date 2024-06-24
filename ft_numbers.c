a37df1bb3df1d4524f21c239d7809d4e7a746f83
ecaleb97 <valley_rob.0c@icloud.com>
2024-06-24 14:53:04 +0200

    chore: Update ft_numbers.c and ft_printf.h

--- ft_numbers.c
+++ ft_numbers.c
@@ -6,7 +6,7 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 22:29:16 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/31 22:40:12 by envillan         ###   ########.fr       */
+/*   Updated: 2024/06/24 11:17:51 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,31 +31,6 @@ void	ft_number(int number, int *length)
 			ft_number(number / 10, length);
 		ft_putchar_length(number % 10 + '0', length);
 	}
-}
-
-void	ft_pointer(size_t pointer, int *length)
-{
-	char	string[25];
-	int		i;
-	char	*base;
-
-	base = "0123456789abcdef";
-	i = 0;
-	write(1, "0x", 2);
-	(*length) += 2;
-	if (pointer == 0)
-	{
-		ft_putchar_length('0', length);
-		return ;
-	}
-	while (pointer != 0)
-	{
-		string[i] = base[pointer % 16];
-		pointer = pointer / 16;
-		i++;
-	}
-	while (i--)
-		ft_putchar_length(string[i], length);
 }
 
 void	ft_hexadecimal(unsigned int x, int *length, char c)
@@ -89,4 +64,29 @@ void	ft_unsigned_int(unsigned int u, int *length)
 	if (u >= 10)
 		ft_unsigned_int(u / 10, length);
 	ft_putchar_length(u % 10 + '0', length);
+}
+
+void	ft_pointer(size_t pointer, int *length)
+{
+	char	string[25];
+	int		i;
+	char	*base;
+
+	base = "0123456789abcdef";
+	i = 0;
+	write(1, "0x", 2);
+	(*length) += 2;
+	if (pointer == 0)
+	{
+		ft_putchar_length('0', length);
+		return ;
+	}
+	while (pointer != 0)
+	{
+		string[i] = base[pointer % 16];
+		pointer = pointer / 16;
+		i++;
+	}
+	while (i--)
+		ft_putchar_length(string[i], length);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: envillan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 22:16:31 by envillan          #+#    #+#             */
-/*   Updated: 2024/05/31 22:54:54 by envillan         ###   ########.fr       */
+/*   Updated: 2024/06/24 11:17:12 by envillan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+int		ft_printf(const char *string, ...);
 void	ft_putchar_length(char c, int *length);
 void	ft_string(char *args, int *length);
 
@@ -24,7 +25,5 @@ void	ft_number(int number, int *lenght);
 void	ft_hexadecimal(unsigned int u, int *length, char c);
 void	ft_unsigned_int(unsigned int u, int *length);
 void	ft_pointer(size_t pointer, int *length);
-
-int		ft_printf(const char *string, ...);
 
 #endif
